f8784a0e72c4508098cee42b2169cf370ee9ef38
lyonel.martinez <lyonel.martinez@epita.fr>
2022-01-31 14:11:42 +0100

    [DM12] Add group for docker

--- install_docker.sh
+++ install_docker.sh
@@ -14,3 +14,6 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo usermod -aG docker ${USER}
+$SHELL
