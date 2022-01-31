36754bde306dc03156b7436b98d93aa5d5ecd7f0
lyonel.martinez <lyonel.martinez@epita.fr>
2022-01-31 13:46:27 +0100

    [DM12] update pip script

--- install_pip_and_deps.sh
+++ install_pip_and_deps.sh
@@ -5,13 +5,16 @@ pip3 --help > /dev/null 2>&1
 
 if [ $? -gt 0 ]; then
     echo 'Installing python3-distutils'
-    sudo apt install python3-distutils
+    sudo apt install python3-distutils -y
     echo 'Curl get-pip.py'
-    curl -sSL https://bootstrap.pypa.io/get-pip.py -o /tmp/git-pip.py
+    curl -sSL https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
     echo 'Exec get-pip.py'
-    python3 /tmp/get-pip.py 2> /dev/null
+    python3 /tmp/get-pip.py
     echo "Moving pip's scripts into /usr/local/bin"
-    sudo mv ~/.local/bin/pip* /usr/local/bin 
+    sudo mv "$HOME/.local/bin/pip" \
+	    "$HOME/.local/bin/pip3" \
+	    "$HOME/.local/bin/pip3.9.2" \
+	    "/usr/local/bin/"
 fi
 echo 'install tweepy json elasticsearch package'
-pip3 install tweepy json elasticsearch
+pip install tweepy elasticsearch 
